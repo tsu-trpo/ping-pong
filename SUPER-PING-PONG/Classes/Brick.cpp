6f04a60b36e30a24daa9f6004ac29fb72c3935b1
MashaSamoylova <mashanedyak@gmail.com>
2017-11-21 09:20:43 +0700

    [+]Brick.cpp + createWithTexture

---
+++ SUPER-PING-PONG/Classes/Brick.cpp
@@ -0,0 +1,9 @@
+#include <Brick.h>
+
+Brick* Brick::createWithTexture(std::string textureName) {
+    Brick* self = new (std::nothrow) Brick();
+    self->initWithFile(textureName);
+    self->autorelease();
+
+    return self;
+}
