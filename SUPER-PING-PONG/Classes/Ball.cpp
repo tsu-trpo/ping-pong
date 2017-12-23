d800c4790297f7b177aadae5b76de0d0851f178e
manmolecular <regwebghost@yandex.ru>
2017-12-23 15:06:24 +0700

    [~] again fix brick delete position

--- SUPER-PING-PONG/Classes/Ball.cpp
+++ SUPER-PING-PONG/Classes/Ball.cpp
@@ -135,10 +135,10 @@ void Ball::onContactWithPaddle(Paddle *paddle)
 void Ball::onContactWithBrick(Brick *brick)
 {
     // Fix physics-bug with left bottom corner
-    brick->deleteBrick();
     if (random(1, 10) == 1)
     {
         runAction(CallFunc::create(CC_CALLBACK_0(Bonus::dropBonus, brick->getPosition())));
     }
+    brick->deleteBrick();
     getEventDispatcher()->dispatchCustomEvent(event::hitBrick);
 }
